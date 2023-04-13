cb27461c6acc2f69bc99ca1379d418b1ba9417da
James Rayner <james@ecreationmedia.tv>
2023-04-13 17:32:11 +0100

    Bump revision to 19

--- board/src/piconet.c
+++ board/src/piconet.c
@@ -17,7 +17,7 @@
 
 #define VERSION_MAJOR           1
 #define VERSION_MINOR           0
-#define VERSION_REV             15
+#define VERSION_REV             19
 #define VERSION_STR_MAXLEN      17
 
 #define TX_DATA_BUFFER_SZ       3500
